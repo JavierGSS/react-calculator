15937c391f5673099cd92fadb2b55a716a648ed0
Baba <jagasal@icloud.com>
2024-04-14 14:56:54 -0600

    edit index.js

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,5 +1,18 @@
 // index.js is the base for file for rendering in React apps.
 
+
+/* import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+); */
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
