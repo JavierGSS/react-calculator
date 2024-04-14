c75392146921d8141e819bc1ca53a46d9836a457
Baba <jagasal@icloud.com>
2024-04-14 15:37:15 -0600

    minor changes

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -12,17 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-/* 
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
- */
