457358d78e586045a8566312b0ff230d8d254850
Baba <jagasal@icloud.com>
2024-04-14 14:07:29 -0600

    edit app/components/Screen.js & Screen.css

---
+++ calculator/src/components/Screen.js
@@ -0,0 +1,17 @@
+/* The Screen component is the top section child of the Wrapper component,
+and its purpose is to display the calculated values.
+We use a small library called react-textfit for display output resizing on length,
+meaning longer values must shrink in size. */
+
+import { Textfit } from "react-textfit";
+import "./Screen.css";
+
+const Screen = ({ value }) => {
+    return (
+      <Textfit className="screen" mode="single" max={70}>
+        {value}
+      </Textfit>
+    );
+  };
+  
+  export default Screen;
