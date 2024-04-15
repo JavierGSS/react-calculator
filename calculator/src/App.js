8caefc5be73592e79b2370701b066f95199125bf
Baba <jagasal@icloud.com>
2024-04-14 19:53:36 -0600

    edit App.js: add onClick functions (1)

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -27,6 +27,67 @@ const App = () => {
     res: 0,
   });
 
+  /* numClickHandler() gets triggered only if any of the number buttons 
+  (0–9) are pressed. Then it gets the value of the Button and adds 
+  that to the current num value. It also makes sure that:
+   * no whole numbers start with zero;
+   * there are no multiple zeros before the comma;
+   * the format will be “0.” if “.” is pressed first; and
+   * numbers are entered up to 16 integers long: */
+
+  const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    console.log(typeof value);
+
+    if (calc.num.length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? 0
+            : calc.num % 1 === 0
+            ? Number(calc.num + value)
+            : calc.num + value,
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+  };
+
+  
+/*   The commaClickHandler function gets fired only if the decimal point (.) is pressed.
+  It adds the decimal point to the current num value, making it a decimal number. 
+  It also makes sure that no multiple decimal points are possible: */
+
+  const commaClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+  
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
+  };
+
+/* The signClickHandler function gets fired when the user press either +, –, * or /. 
+  The particular value is then set as a current sign value in the calc object.
+  It also makes sure there’s no effect on repeated calls: */
+
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+  
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    });
+  };
+
+
+
+
   return (
     <Wrapper>
       <Screen value={calc.num ? calc.num : calc.res} />
